1e73c46f940980bc6e93abd14b908d210a72ca14
Niels Andriesse <andriesseniels@gmail.com>
2020-03-04 12:04:41 +1100

    WIP

--- Signal/src/Loki/Components/VoiceMessageView.swift
+++ Signal/src/Loki/Components/VoiceMessageView.swift
@@ -11,8 +11,12 @@ final class VoiceMessageView : UIView {
     }()
     
     private var isDownloading: Bool {
-        guard let attachmentPointer = self.viewItem.attachmentPointer, attachmentPointer.uniqueId != nil else { return false }
+        guard let attachmentPointer = viewItem.attachmentPointer, attachmentPointer.uniqueId != nil else { return false }
         return attachmentPointer.state == .enqueued || attachmentPointer.state == .downloading
+    }
+    
+    private var isPlaying: Bool {
+        return viewItem.audioPlaybackState() == .playing
     }
 
     // MARK: Settings
@@ -100,7 +104,7 @@ final class VoiceMessageView : UIView {
     }
 
     private func setUpViewHierarchy() {
-        let leftViewInset = ProgressView.lineThickness / 2
+        let leftViewInset = ProgressView.lineThickness
         leftView.addSubview(playbackButton)
         playbackButton.pin(to: leftView, withInset: leftViewInset)
         leftView.addSubview(progressView)
@@ -118,6 +122,9 @@ final class VoiceMessageView : UIView {
     @objc func update() {
         let verticalMargin = VoiceMessageView.getVerticalMargin(for: viewItem)
         stackView.layoutMargins = UIEdgeInsets(top: verticalMargin, left: 0, bottom: verticalMargin, right: 0)
+        let icon = isPlaying ? #imageLiteral(resourceName: "CirclePause") : #imageLiteral(resourceName: "CirclePlay")
+        playbackButton.setImage(icon, for: UIControl.State.normal)
+        progressView.fraction = viewItem.audioProgressSeconds / viewItem.audioDurationSeconds
         if isDownloading {
             waveformView.alpha = 0
             spinner.startAnimating()
@@ -128,7 +135,10 @@ final class VoiceMessageView : UIView {
                 }
             }
             let url = Bundle.main.url(forResource: "file_example_MP3_2MG", withExtension: "mp3")!
-            waveformView.play(from: url)
+            AudioContext.load(fromAudioURL: url) { [weak self] audioContext in
+                guard let audioContext = audioContext else { return }
+                self?.waveformView.meteringLevels = audioContext.render(targetSamples: 100)
+            }
             if spinner.alpha == 1 {
                 UIView.animate(withDuration: 0.25) {
                     self.spinner.alpha = 0
@@ -141,7 +151,8 @@ final class VoiceMessageView : UIView {
 
 // MARK: Progress View
 private final class ProgressView : UIView {
-
+    var fraction: CGFloat = 0 { didSet { updateLayers() } }
+        
     static let lineThickness = CGFloat(2)
     
     private lazy var bottomLineLayer: CAShapeLayer = {
@@ -159,6 +170,12 @@ private final class ProgressView : UIView {
         result.fillColor = UIColor.clear.cgColor
         return result
     }()
+    
+    private lazy var dotLayer: CAShapeLayer = {
+        let result = CAShapeLayer()
+        result.fillColor = Colors.accent.cgColor
+        return result
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -173,19 +190,25 @@ private final class ProgressView : UIView {
     private func setUpViewHierarchy() {
         layer.insertSublayer(bottomLineLayer, at: 0)
         layer.insertSublayer(topLineLayer, at: 1)
+        layer.insertSublayer(dotLayer, at: 2)
     }
 
     // MARK: Updating
     override func layoutSubviews() {
         super.layoutSubviews()
-        updateLineLayer()
+        updateLayers()
     }
 
-    private func updateLineLayer() {
-        let bottomPath = UIBezierPath(ovalIn: bounds)
-        let fraction = CGFloat(0.75)
-        let topPath = UIBezierPath(arcCenter: bounds.center, radius: width() / 2, startAngle: .pi, endAngle: .pi + fraction * 2 * .pi, clockwise: true)
-        bottomLineLayer.path = bottomPath.cgPath
-        topLineLayer.path = topPath.cgPath
+    private func updateLayers() {
+        let bottomLinePath = UIBezierPath(ovalIn: bounds)
+        let r = width() / 2
+        let θ = .pi + fraction * 2 * .pi
+        let topLinePath = UIBezierPath(arcCenter: bounds.center, radius: r, startAngle: .pi, endAngle: θ, clockwise: true)
+        let dotPathCenterX = bounds.center.x + r * cos(θ)
+        let dotPathCenterY = bounds.center.y + r * sin(θ)
+        let dotPath = UIBezierPath(arcCenter: CGPoint(x: dotPathCenterX, y: dotPathCenterY), radius: 2, startAngle: 0, endAngle: 2 * .pi, clockwise: false)
+        bottomLineLayer.path = bottomLinePath.cgPath
+        topLineLayer.path = topLinePath.cgPath
+        dotLayer.path = dotPath.cgPath
     }
 }
